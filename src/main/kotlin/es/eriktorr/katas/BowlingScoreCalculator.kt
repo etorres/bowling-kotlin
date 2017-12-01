abe4ad24f2d7372ad5e1e7f3f5eb620d0a6eb765
Erik Torres <erik.torres@codurance.com>
2017-12-01 20:19:08 +0100

    upgrade kotlin version to 1.2.0

--- src/main/kotlin/es/eriktorr/katas/BowlingScoreCalculator.kt
+++ src/main/kotlin/es/eriktorr/katas/BowlingScoreCalculator.kt
@@ -2,6 +2,18 @@ package es.eriktorr.katas
 
 class BowlingScoreCalculator {
 
-    fun scoreFrom(rolls: String): Int = 0
+    companion object {
+        val FRAMES_TOTAL = 10
+    }
+
+    fun scoreOf(rolls: String): Int {
+        val frames = framesFrom(rolls)
+
+        println("$frames");
+
+        return 0
+    }
+
+    private fun framesFrom(rolls: String) = rolls.split("\\s".toRegex(), FRAMES_TOTAL)
 
 }
--- src/test/kotlin/es/eriktorr/katas/BowlingScoreCalculatorTest.kt
+++ src/test/kotlin/es/eriktorr/katas/BowlingScoreCalculatorTest.kt
@@ -1,7 +1,7 @@
 package es.eriktorr.katas
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
 
 class BowlingScoreCalculatorTest {
@@ -10,12 +10,12 @@ class BowlingScoreCalculatorTest {
 
     @TestFactory
     fun calculateGameScore() = listOf(
-            "X X X X X X X X X X X X" to 300,
+            "X X X X X X X X X XXX" to 300,
             "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-" to 90,
             "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5" to 150)
             .map { (rolls, gameScore) ->
-                DynamicTest.dynamicTest("[$rolls] => $gameScore") {
-                    assertThat(bowlingScoreCalculator.scoreFrom(rolls)).isEqualTo(gameScore)
+                dynamicTest("[$rolls] => $gameScore") {
+                    assertThat(bowlingScoreCalculator.scoreOf(rolls)).isEqualTo(gameScore)
                 }
             }
 
